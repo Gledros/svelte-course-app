71f22373ad5e81df334fce8a24672520e47ef5f7
Glenn Della Rocca <glenn@dellarocca.mx>
2023-12-19 14:51:43 -0700

    modify Counter calling
    Using Svelte shorthand for props

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,13 @@
   import Counter from './lib/Counter.svelte';
 
   let name = 'Svelte';
+
+  const maxCount = 6;
 </script>
 
 <h1>Hello {name.toLocaleLowerCase()}!</h1>
 
-<Counter initialCount={3} maxCount={10} />
+<Counter initialCount={3} {maxCount} />
 
 <style>
   h1 {
