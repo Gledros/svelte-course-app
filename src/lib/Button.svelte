766f790ca499508bfd9b9c0d05d6f645b8efc7d3
Glenn Della Rocca <glenn@dellarocca.mx>
2023-12-19 15:54:11 -0700

    add styling to Button component

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -2,3 +2,13 @@
 </script>
 
 <button><slot>Hey, listen!</slot></button>
+
+<style>
+  button {
+    border: none;
+    background-color: green;
+    color: whitesmoke;
+    font-weight: bold;
+    padding: 0.5rem 1.5rem;
+  }
+</style>
