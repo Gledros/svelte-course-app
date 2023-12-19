096d7633b6f5b9a1ded1f5a5b6bb7bbfd4339a04
Glenn Della Rocca <glenn@dellarocca.mx>
2023-12-19 14:39:29 -0700

    modify reactive statements
    Created an example of why the reactive statement's order is important for the correct functioning of the code.

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -2,27 +2,21 @@
   let count1 = 0;
   let count2 = 0;
 
-  let string;
+  let string = 'Count2 is 0';
+
+  const setCount2 = (value) => (count2 = value);
 
   /*
-    According to course instructor using the functions wouldn't work for
-    reactive statements unless they got the states inside them, but using both
-    kind of functions work as expected
+    The order of the reactive statements it's important. Reversing the following
+    two lines makes the assignment of 'string' variable unaccounted for.
   */
-  const getTotalCount = () => count1 + count2;
-  function getTotalCount2() {
-    return count1 + count2;
-  }
-
-  $: if (count1 + count2 > 5) string = `The count total is ${getTotalCount()}`;
-  $: if (count1 + count2 == 0) string = 'The count total is less than 5';
+  $: setCount2(count1);
+  $: string = `Count2 is ${count2}`;
 
   const increment1 = () => (count1 += 1);
-  const increment2 = () => (count2 += 1);
   const reset = () => (count1 = count2 = 0);
 </script>
 
 <button on:click={increment1}>Clicks {count1}</button>
-<button on:click={increment2}>Clicks {count2}</button>
 <h3>{string}</h3>
 <button on:click={reset}>Reset count</button>
