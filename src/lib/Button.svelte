61d5a5ce47485a5d289ca418bfa99eccd56fff94
Glenn Della Rocca <glenn@dellarocca.mx>
2023-12-19 16:10:16 -0700

    install  & implement sass dependency
    Added an example inside the styling of the Button component

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -3,12 +3,21 @@
 
 <button><slot>Hey, listen!</slot></button>
 
-<style>
+<style lang="scss">
   button {
     border: none;
-    background-color: green;
+    background-color: #008000;
     color: whitesmoke;
     font-weight: bold;
     padding: 0.5rem 1.5rem;
+
+    transition:
+      background-color 1.5s ease,
+      color 0.25s ease;
+
+    &:hover {
+      background-color: #19cd19;
+      color: black;
+    }
   }
 </style>
