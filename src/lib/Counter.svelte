43df588a7c73aa00d40ce20fcb02b74f43aac5c6
Glenn Della Rocca <hola@glenndellarocca.mx>
2023-12-19 14:02:07 -0700

    add more reactive statements
    Combined statements and tested the supposed problems with reactivity of Svelte

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,15 +1,28 @@
 <script>
-  let count = 0;
+  let count1 = 0;
+  let count2 = 0;
 
   let string;
 
-  $: if (count > 5) string = `You have clicked ${count}`;
-  $: if (count == 0) string = 'You have clicked less than 5';
+  /*
+    According to course instructor using the functions wouldn't work for
+    reactive statements unless they got the states inside them, but using both
+    kind of functions work as expected
+  */
+  const getTotalCount = () => count1 + count2;
+  function getTotalCount2() {
+    return count1 + count2;
+  }
 
-  const increment = () => count++;
-  const reset = () => (count = 0);
+  $: if (count1 + count2 > 5) string = `The count total is ${getTotalCount()}`;
+  $: if (count1 + count2 == 0) string = 'The count total is less than 5';
+
+  const increment1 = () => (count1 += 1);
+  const increment2 = () => (count2 += 1);
+  const reset = () => (count1 = count2 = 0);
 </script>
 
-<button on:click={increment}>Clicks {count}</button>
+<button on:click={increment1}>Clicks {count1}</button>
+<button on:click={increment2}>Clicks {count2}</button>
 <h3>{string}</h3>
 <button on:click={reset}>Reset count</button>
