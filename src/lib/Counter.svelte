d31fd68b7151a629f70d8678f2b81f7a139b7932
Glenn Della Rocca <hola@glenndellarocca.mx>
2023-12-19 13:09:41 -0700

    implement reactive statement

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,7 +1,15 @@
 <script>
-	let count = 0
+  let count = 0;
 
-	const increment = () => count++
+  let string;
+
+  $: if (count > 5) string = `You have clicked ${count}`;
+  $: if (count == 0) string = 'You have clicked less than 5';
+
+  const increment = () => count++;
+  const reset = () => (count = 0);
 </script>
 
 <button on:click={increment}>Clicks {count}</button>
+<h3>{string}</h3>
+<button on:click={reset}>Reset count</button>
