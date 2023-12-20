a553bb6981a24b4cf71606995a406414a9461480
Glenn Della Rocca <glenn@dellarocca.mx>
2023-12-20 12:56:43 -0700

    implement sass variables

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -6,17 +6,17 @@
 <style lang="scss">
   button {
     border: none;
-    background-color: #008000;
+    background-color: variables.$color_primary;
     color: whitesmoke;
     font-weight: bold;
     padding: 0.5rem 1.5rem;
 
     transition:
       background-color 1.5s ease,
-      color 0.25s ease;
+      color 0.5s ease;
 
     &:hover {
-      background-color: #19cd19;
+      background-color: variables.$color_secondary;
       color: black;
     }
   }
