71e534c57706508c74866416b2b67d3ff16f336e
Glenn Della Rocca <glenn@dellarocca.mx>
2023-12-27 17:47:48 -0700

    add TodoList component

---
+++ src/lib/TodoList.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let todos;
+</script>
+
+<ul>
+  {#each todos as todo, index (todo.id)}
+    <li>{index + 1} > {todo.title}</li>
+  {/each}
+</ul>
+
+<style>
+  li {
+    list-style-type: none;
+  }
+</style>
