b6a8773c4f843a1f864b668771846de0e1c48a7a
Glenn Della Rocca <glenn@dellarocca.mx>
2023-12-28 09:52:14 -0700

    implement binding component props

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,31 @@
 <script>
   import TodoList from './lib/TodoList.svelte';
+  import { v4 as uuid } from 'uuid';
+
+  let todos = [
+    {
+      id: uuid(),
+      title: 'Todo 1',
+      completed: false,
+    },
+    {
+      id: uuid(),
+      title: 'Todo 2',
+      completed: false,
+    },
+    {
+      id: uuid(),
+      title: 'Todo 3',
+      completed: false,
+    },
+    {
+      id: uuid(),
+      title: 'Todo 4',
+      completed: false,
+    },
+  ];
+
+  $: console.log(todos.length);
 </script>
 
-<TodoList />
+<TodoList bind:todos />
