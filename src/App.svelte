80c70e6fee4dd84bc445041a3fe3f97a98457720
Glenn Della Rocca <glenn@dellarocca.mx>
2024-01-05 13:29:16 -0700

    use await block to fetch data

--- src/App.svelte
+++ src/App.svelte
@@ -4,28 +4,22 @@
   import Button from './lib/Button.svelte';
   import TodoList from './lib/TodoList.svelte';
   import { v4 as uuid } from 'uuid';
-  import { tick } from 'svelte';
 
   let todoList;
   let displayList = true;
 
-  let todos = [
-    {
-      id: uuid(),
-      title: 'Todo 1',
-      completed: false,
-    },
-    {
-      id: uuid(),
-      title: 'Todo 2',
-      completed: true,
-    },
-    {
-      id: uuid(),
-      title: 'Todo 3',
-      completed: true,
-    },
-  ];
+  let todos = [];
+
+  const loadTodos = () => {
+    return fetch('https://jsonplaceholder.typicode.com/todos?_limit=10').then(
+      (response) => {
+        if (response.ok) return response.json();
+        else throw new Error('An error has ocurred');
+      },
+    );
+  };
+
+  let promise = loadTodos();
 
   const handleAddTodo = (event) => {
     todos = [
@@ -56,14 +50,25 @@
 
 <div>
   {#if displayList}
-    <TodoList
-      {todos}
-      bind:this={todoList}
-      on:addTodo={handleAddTodo}
-      on:clearTodos={clearTodos}
-      on:removeTodo={removeTodo}
-      on:toggleTodo={toggleTodo}
-    />
+    {#await promise}
+      <p>Fetching data...</p>
+    {:then todos}
+      <TodoList
+        {todos}
+        bind:this={todoList}
+        on:addTodo={handleAddTodo}
+        on:clearTodos={clearTodos}
+        on:removeTodo={removeTodo}
+        on:toggleTodo={toggleTodo}
+      />
+    {:catch error}
+      <p>{error.message || 'An error has ocurred'}</p>
+    {/await}
+    <Button
+      on:click={() => {
+        promise = loadTodos();
+      }}>Refresh data</Button
+    >
   {/if}
 
   <Button on:click={() => (displayList = !displayList)}>
