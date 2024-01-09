1754e52d40cab8b26ab3ce05b7e68a82fbb2e5f3
Glenn Della Rocca <glenn@dellarocca.mx>
2024-01-09 13:04:03 -0700

    create custom css animation: spin

---
+++ src/lib/transitions/spin.js
@@ -0,0 +1,28 @@
+import { cubicInOut } from 'svelte/easing';
+
+export const spin = (
+  node,
+  { delay = 0, duration = 500, easing = cubicInOut, spin = 1 },
+) => {
+  const originalOpacity = +getComputedStyle(node).opacity;
+  const originalTransform = getComputedStyle(node).transform.replace(
+    'none',
+    '',
+  );
+
+  return {
+    delay,
+    duration,
+    easing,
+    css: (t) => {
+      return `
+        opacity: ${t * originalOpacity};
+        transform: ${originalTransform} scale(${t}) rotate(${
+        spin * 360 * t
+      }deg);
+      `;
+    },
+  };
+};
+
+export default spin;
