fe8673718514eaa2b704632aef8c723ed3b3fe15
Glenn Della Rocca <glenn@dellarocca.mx>
2024-01-12 11:21:34 -0700

    modify longpress action to dispatch customEvent

--- src/lib/actions/longpress.js
+++ src/lib/actions/longpress.js
@@ -1,12 +1,32 @@
 export const longpress = (node, options) => {
-  console.log(node, options);
+  let timer;
+
+  const handleMouseDown = () => {
+    const { duration = 2000 } = options;
+
+    console.log(options, duration);
+    timer = setTimeout(() => {
+      node.dispatchEvent(new CustomEvent('longpress'));
+    }, duration);
+  };
+
+  const handleMouseUp = () => {
+    clearTimeout(timer);
+  };
+
+  node.addEventListener('mousedown', handleMouseDown);
+  node.addEventListener('mouseup', handleMouseUp);
 
   return {
     update: (newOptions) => {
       console.log(newOptions);
+      options = newOptions;
     },
     destroy: () => {
       console.log('destroyed');
+      clearTimeout(timer);
+      node.removeEventListener('mousedown', handleMouseDown);
+      node.removeEventListener('mouseup', handleMouseUp);
     },
   };
 };
