e99a47f9b4e9238ba729858a01156009f30b28e6
Glenn Della Rocca <glenn@dellarocca.mx>
2024-01-12 11:06:37 -0700

    create longpress Svelte action

---
+++ src/lib/actions/longpress.js
@@ -0,0 +1,14 @@
+export const longpress = (node, options) => {
+  console.log(node, options);
+
+  return {
+    update: (newOptions) => {
+      console.log(newOptions);
+    },
+    destroy: () => {
+      console.log('destroyed');
+    },
+  };
+};
+
+export default longpress;
