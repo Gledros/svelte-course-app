85e12c5f968d0b2c75d3eb46227a0dab7d4c4796
Glenn Della Rocca <glenn@dellarocca.mx>
2024-01-18 12:46:55 -0700

    modify Settings page

--- src/lib/pages/Settings.svelte
+++ src/lib/pages/Settings.svelte
@@ -1,41 +1,34 @@
 <script>
+  // @ts-ignore
+  import FaLightbulb from 'svelte-icons/fa/FaLightbulb.svelte';
   import settings from '../stores/settings';
 </script>
 
 <h2>Settings</h2>
 
-{$settings.colorScheme}
-
 <button
   on:click={() => {
     $settings.colorScheme =
       $settings.colorScheme === 'light' ? 'dark' : 'light';
-  }}>Toggle Color Scheme</button
+  }}
 >
-
-<form>
-  <label for="radio1">
-    <input
-      type="radio"
-      id="radio1"
-      value="dark"
-      bind:group={$settings.colorScheme}
-    /> Dark
-  </label>
-  <label for="radio2">
-    <input
-      type="radio"
-      id="radio2"
-      value="light"
-      bind:group={$settings.colorScheme}
-    /> Light
-  </label>
-</form>
+  <FaLightbulb />
+  <p>{$settings.colorScheme} theme</p>
+</button>
 
 <style>
-  form {
-    border: 1px solid black;
-    border-radius: 0.25rem;
-    padding: 1rem;
+  button {
+    display: flex;
+    background: none;
+    border: 2px solid var(--textColor);
+    padding: 0.75rem;
+    height: 3.5rem;
+    color: var(--textColor);
+    align-items: center;
+  }
+
+  p {
+    text-align: left;
+    font-weight: bold;
   }
 </style>
