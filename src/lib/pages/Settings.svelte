31102c88db79eb46f17c95ba9775af7a34d22b86
Glenn Della Rocca <glenn@dellarocca.mx>
2024-01-18 10:16:57 -0700

    implement Svelte store auto-suscription & store binding

--- src/lib/pages/Settings.svelte
+++ src/lib/pages/Settings.svelte
@@ -1,39 +1,33 @@
 <script>
-  import { onDestroy } from 'svelte';
   import settings from '../stores/settings';
-
-  let colorScheme;
-
-  const unsubscribe = settings.subscribe((settings) => {
-    colorScheme = settings.colorScheme;
-  });
-
-  $: settings.update((prevSettings) => ({ ...prevSettings, colorScheme }));
-
-  onDestroy(unsubscribe);
 </script>
 
 <h2>Settings</h2>
 
-{colorScheme}
+{$settings.colorScheme}
+
+<button
+  on:click={() => {
+    $settings.colorScheme =
+      $settings.colorScheme === 'light' ? 'dark' : 'light';
+  }}>Toggle Color Scheme</button
+>
 
 <form>
   <label for="radio1">
     <input
       type="radio"
       id="radio1"
-      name="colorScheme"
       value="dark"
-      bind:group={colorScheme}
+      bind:group={$settings.colorScheme}
     /> Dark
   </label>
   <label for="radio2">
     <input
       type="radio"
       id="radio2"
-      name="colorScheme"
       value="light"
-      bind:group={colorScheme}
+      bind:group={$settings.colorScheme}
     /> Light
   </label>
 </form>
