01404504d52cbada1363bf8129de5205c6f81d24
Glenn Della Rocca <glenn@dellarocca.mx>
2024-01-26 11:14:12 -0700

    transform settings store to a custom store
    A custom Svelte store needs to implement only the subscribe method, all others are optional, and also there can be other custom methods for easier work with the store

--- src/lib/stores/settings.js
+++ src/lib/stores/settings.js
@@ -1,9 +1,34 @@
 import { writable } from 'svelte/store';
 
-const settings = writable({
-  colorScheme: 'dark',
-  language: 'english',
-  fontSize: 12,
-});
+function createSettingsStore() {
+  const defaultSettings = {
+    colorScheme: 'dark',
+    language: 'english',
+    fontSize: 12,
+  };
 
-export default settings;
+  const { subscribe, set, update } = writable(defaultSettings);
+
+  return {
+    subscribe,
+    set,
+    update,
+    updateSetting: (setting, value) => {
+      update((settings) => ({
+        ...settings,
+        [setting]: value,
+      }));
+    },
+    toggleColorScheme: () => {
+      update((settings) => ({
+        ...settings,
+        colorScheme: settings.colorScheme === 'dark' ? 'light' : 'dark',
+      }));
+    },
+    reset: () => {
+      set(defaultSettings);
+    },
+  };
+}
+
+export default createSettingsStore();
