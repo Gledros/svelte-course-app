d0fbbdf217368e09783c0b0912ee0ce4a7dcd308
Glenn Della Rocca <glenn@dellarocca.mx>
2024-01-26 12:35:18 -0700

    create & implement a tweened Svelte store

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,7 +1,25 @@
 <script>
-  import location from '../stores/location';
+  import { cubicOut } from 'svelte/easing';
+  import { tweened } from 'svelte/motion';
+
+  const size = tweened(1, {
+    duration: 200,
+    easing: cubicOut,
+  });
 </script>
 
 <h2>Home</h2>
 
-{$location}
+<button on:click={() => ($size += 0.5)}>Bigger</button>
+<button on:click={() => ($size -= 0.5)}>Smaller</button>
+
+<div style="transform: scale({$size})"></div>
+
+<style>
+  div {
+    width: 5rem;
+    height: 5rem;
+    background-color: rebeccapurple;
+    transform-origin: 0 0;
+  }
+</style>
