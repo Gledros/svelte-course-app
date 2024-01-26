94de9410a210a7e2bd0da2c488d9b61a0c7dcfda
Glenn Della Rocca <glenn@dellarocca.mx>
2024-01-26 12:42:30 -0700

    modify tweened store
    Changed the scalar to an object with 2 values to make the width and height random

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -2,23 +2,34 @@
   import { cubicOut } from 'svelte/easing';
   import { tweened } from 'svelte/motion';
 
-  const size = tweened(1, {
-    duration: 200,
-    easing: cubicOut,
-  });
+  const size = tweened(
+    { width: 5, height: 5 },
+    {
+      duration: 1000,
+      easing: cubicOut,
+    },
+  );
 </script>
 
 <h2>Home</h2>
 
-<button on:click={() => ($size += 0.5)}>Bigger</button>
-<button on:click={() => ($size -= 0.5)}>Smaller</button>
+<button
+  on:click={async () => {
+    await size.set({
+      width: Math.random() * 20,
+      height: Math.random() * 20,
+    });
 
-<div style="transform: scale({$size})"></div>
+    console.log('done'); // Only gets executed when the promise gets resolved; if it gets cancelled/interrupted it does not execute
+  }}>Random size</button
+>
+
+<div
+  style="width:{$size.width}rem;height:{$size.height}rem;transform: scale({$size})"
+></div>
 
 <style>
   div {
-    width: 5rem;
-    height: 5rem;
     background-color: rebeccapurple;
     transform-origin: 0 0;
   }
