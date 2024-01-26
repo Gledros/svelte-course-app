7f1254696f9e7c13c419a7357f0a371d9340d193
Glenn Della Rocca <glenn@dellarocca.mx>
2024-01-26 13:00:13 -0700

    implement custom interpolation for tweened store
    The tweened store can only work with numbers, but with the custom interpolation it's possible to use it with strings or other data types.
    The interpolateRgb of d3-interpolate is the function that does the magic of interpolation to get that smooth change in color

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,12 +1,24 @@
 <script>
   import { cubicOut } from 'svelte/easing';
   import { tweened } from 'svelte/motion';
+  import { interpolateRgb } from 'd3-interpolate';
 
-  const size = tweened(
-    { width: 5, height: 5 },
+  const boxProps = tweened(
+    { width: 5, height: 5, color: 'rebeccapurple' },
     {
       duration: 1000,
       easing: cubicOut,
+      interpolate: (a, b) => (t) => {
+        const deltaWidth = b.width - a.width;
+        const deltaHeight = b.height - a.height;
+        const color = interpolateRgb(a.color, b.color)(t);
+
+        return {
+          width: a.width + deltaWidth * t,
+          height: a.height + deltaHeight * t,
+          color,
+        };
+      },
     },
   );
 </script>
@@ -15,22 +27,16 @@
 
 <button
   on:click={async () => {
-    await size.set({
+    await boxProps.set({
       width: Math.random() * 20,
       height: Math.random() * 20,
+      color: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
     });
 
     console.log('done'); // Only gets executed when the promise gets resolved; if it gets cancelled/interrupted it does not execute
-  }}>Random size</button
+  }}>Random box</button
 >
 
 <div
-  style="width:{$size.width}rem;height:{$size.height}rem;transform: scale({$size})"
+  style="transform-origin: 0 0; width:{$boxProps.width}rem; height:{$boxProps.height}rem; transform: scale({$boxProps}); background-color:{$boxProps.color}"
 ></div>
-
-<style>
-  div {
-    background-color: rebeccapurple;
-    transform-origin: 0 0;
-  }
-</style>
