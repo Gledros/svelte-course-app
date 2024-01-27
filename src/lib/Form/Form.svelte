dcd7fbda1abde09e70047b79699c4abfe02c3d1b
Glenn Della Rocca <glenn@dellarocca.mx>
2024-01-26 17:29:05 -0700

    create Form component
    Added context 'form'

---
+++ src/lib/Form/Form.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { setContext } from 'svelte';
+  export let initialValues = {};
+
+  setContext('form', { values: initialValues, errors: {} });
+</script>
+
+<form>
+  <slot />
+</form>
+
+<style>
+  form {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 0.5rem;
+    justify-items: center;
+  }
+</style>
