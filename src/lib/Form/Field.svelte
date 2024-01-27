cc7b92db3dd975da58bb3941a38f1a3b6d1ae789
Glenn Della Rocca <glenn@dellarocca.mx>
2024-01-26 17:29:29 -0700

    create Field component
    Uses 'form' context

---
+++ src/lib/Form/Field.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { getContext } from 'svelte';
+  import { v4 as uuid } from 'uuid';
+
+  export let name;
+  export let label = undefined;
+  export let placeholder = label;
+  export let type = 'text';
+  export let validate = undefined;
+
+  const { values } = getContext('form');
+
+  const id = uuid();
+</script>
+
+{#if label}
+  <label for={id}>{label}:</label>
+{/if}
+<input {type} {name} {id} {placeholder} value={values[name] || ''} />
+
+<style>
+  label {
+    width: 100%;
+    text-align: end;
+  }
+</style>
