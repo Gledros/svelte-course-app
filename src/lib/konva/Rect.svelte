e2c70ae37d4959f83e3dbe073c9eef5d1c95b601
Glenn Della Rocca <glenn@dellarocca.mx>
2024-02-28 15:21:08 -0700

    add Rect wrapper component

---
+++ src/lib/konva/Rect.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Konva from 'konva';
+  import { getContext } from 'svelte';
+  import { layerKey } from './context-keys';
+
+  export let x, y, fill, width, height, stroke, strokeWidth;
+
+  const layer = getContext(layerKey);
+
+  const rect = new Konva.Rect({
+    x,
+    y,
+    width,
+    height,
+    fill,
+    stroke,
+    strokeWidth,
+  });
+
+  layer.add(rect);
+</script>
