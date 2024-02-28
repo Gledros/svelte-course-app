01d4dbb041f35e232d73b361d382b3011f4bab60
Glenn Della Rocca <glenn@dellarocca.mx>
2024-02-28 15:22:38 -0700

    implement Konva wrappers for declarative use of the dependency

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,50 +1,42 @@
 <script>
-  import { onMount } from 'svelte';
-  import Konva from 'konva';
+  import Layer from '../konva/Layer.svelte';
+  import Rect from '../konva/Rect.svelte';
+  import Stage from '../konva/Stage.svelte';
 
-  let container;
-
-  onMount(() => {
-    const stage = new Konva.Stage({
-      container,
-      width: 300,
-      height: 350,
-    });
-
-    const layer = new Konva.Layer();
-    const rect = new Konva.Rect({
-      x: 20,
-      y: 20,
-      width: 100,
-      height: 60,
-      fill: '#138168',
-      strokeWidth: 4,
-    });
-
-    const rect2 = new Konva.Rect({
-      x: 140,
-      y: 20,
-      width: 50,
-      height: 160,
-      fill: 'aqua',
-      strokeWidth: 0,
-      shadowColor: 'rgba(1,1,1,0.5)',
-      shadowBlur: 10,
-      cornerRadius: 5,
-    });
-
-    layer.add(rect);
-    layer.add(rect2);
-    stage.add(layer);
-  });
+  let showCanvas = true;
 </script>
 
 <h2>Home</h2>
 
-<div bind:this={container} />
+<label for="showCanvas"
+  ><input id="showCanvas" type="checkbox" bind:checked={showCanvas} /> Display canvas</label
+>
 
-<style>
-  div {
-    background-color: white;
+{#if showCanvas}
+  <Stage width={300} height={350}>
+    <Layer>
+      <Rect
+        x={20}
+        y={20}
+        fill="blue"
+        width={100}
+        height={100}
+        stroke={undefined}
+        strokeWidth={4}
+      />
+    </Layer>
+  </Stage>
+{/if}
+
+<style lang="scss">
+  label {
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    &:hover {
+      color: variables.$color_primary;
+    }
   }
 </style>
