ee6a1e907615afaf797f1a2054e0c266e9c0598c
Glenn Della Rocca <glenn@dellarocca.mx>
2024-02-29 10:21:55 -0700

    implement Konva fixes

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -12,9 +12,9 @@
   ><input id="showCanvas" type="checkbox" bind:checked={showCanvas} /> Display canvas</label
 >
 
-{#if showCanvas}
-  <Stage width={300} height={350}>
-    <Layer>
+<Stage width={300} height={350}>
+  {#if showCanvas}
+    <Layer draggable>
       <Rect
         x={20}
         y={20}
@@ -24,9 +24,29 @@
         stroke={undefined}
         strokeWidth={4}
       />
+      <Rect
+        x={40}
+        y={40}
+        fill="green"
+        width={100}
+        height={100}
+        stroke={undefined}
+        strokeWidth={4}
+      />
     </Layer>
-  </Stage>
-{/if}
+  {/if}
+  <Layer>
+    <Rect
+      x={60}
+      y={60}
+      fill="orange"
+      width={100}
+      height={100}
+      stroke="purple"
+      strokeWidth={4}
+    />
+  </Layer>
+</Stage>
 
 <style lang="scss">
   label {
