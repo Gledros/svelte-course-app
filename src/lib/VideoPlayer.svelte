1e27a5364bd04f1c2ca748775bb7ad93d429c8f7
Glenn Della Rocca <glenn@dellarocca.mx>
2024-03-04 11:50:51 -0700

    add & implement VideoPlayer component

---
+++ src/lib/VideoPlayer.svelte
@@ -0,0 +1,52 @@
+<script>
+  import Button from './Button.svelte';
+
+  export let src = undefined;
+
+  let paused = true,
+    currentTime = 0,
+    volume = 0;
+</script>
+
+<div>
+  <!-- svelte-ignore a11y-media-has-caption -->
+  <video
+    class:playing={!paused}
+    {src}
+    controls
+    muted
+    bind:paused
+    bind:volume
+    bind:currentTime
+  />
+
+  <span>
+    <Button
+      on:click={() => {
+        paused = !paused;
+      }}
+    >
+      {paused ? 'Play' : 'Pause'}
+    </Button>
+
+    <p>{currentTime.toFixed(2)}</p>
+    <p>{(volume * 100).toFixed(0)}</p>
+  </span>
+</div>
+
+<style>
+  div {
+    display: flex;
+  }
+
+  span {
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+
+  video.playing {
+    outline: 0.25rem solid orange;
+  }
+</style>
--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,44 +1,12 @@
 <script>
-  import Button, { getTotalButtons } from '../Button.svelte';
-
-  let showButton = false;
-  let total = 0;
+  import VideoPlayer from '../VideoPlayer.svelte';
 </script>
 
 <h2>Home</h2>
 
-<label for="showButtonCheckbox">
-  <input id="showButtonCheckbox" type="checkbox" bind:checked={showButton} />
-  Show Button
-</label>
-
-<Button>Button</Button>
-<Button>Button</Button>
-
-{#if showButton}
-  <Button>Button</Button>
-{/if}
-
-<Button
-  on:click={() => {
-    total = getTotalButtons();
-  }}>Update count : {total}</Button
->
+<VideoPlayer src="mov_bbb.mp4" />
+<VideoPlayer src="mov_bbb.mp4" />
+<VideoPlayer src="mov_bbb.mp4" />
 
 <style lang="scss">
-  label {
-    font-weight: bold;
-    display: flex;
-    align-items: center;
-    gap: 0.5rem;
-
-    &:hover {
-      cursor: pointer;
-      color: variables.$color_primary;
-    }
-  }
-
-  [type='checkbox']:hover {
-    cursor: pointer;
-  }
 </style>
